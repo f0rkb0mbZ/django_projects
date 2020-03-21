624dd311bbd04d4c3a83466e5ece4f0e0dabe5fa
Snehangshu Bhattacharya <snehangshubhattacharya@yahoo.in>
2020-03-21 20:16:55 +0530

    Initial Commit

---
+++ django_for_apis/library_project/books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+# class Book(models.Model)
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("book_detail", kwargs={"pk": self.pk})
